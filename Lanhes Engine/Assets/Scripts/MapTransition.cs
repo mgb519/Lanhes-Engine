b87859f28f2ccc6be32f2f2d3e2f3e9524acd579
mgb519 <mgb519@aol.com>
2019-06-19 08:01:52 -0500

    Added scene transitions

---
+++ Lanhes Engine/Assets/Scripts/MapTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    public string sceneToLoad;
+    SceneManager manager;
+    GameObject player;
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<SceneManager>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        print("Check");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Check");
+        if (collision.gameObject == player) {
+            print("CheckAgain");
+            manager.StartLoadScene(sceneToLoad);
+        }
+    }
+}

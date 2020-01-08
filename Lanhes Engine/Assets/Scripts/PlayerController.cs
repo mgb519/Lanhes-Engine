c7505e9a5ab934e88f05962b205175c3e6f130dd
theNimja <theNimja@github.com>
2020-01-08 13:40:44 +0000

    Basic contonius 3d world movement implemented

--- Lanhes Engine/Assets/Scripts/PlayerController.cs
+++ Lanhes Engine/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public Waypoint position;
-    private Waypoint target;
-    public Vector2Int facing;
-
-    private Map map;
-
-  
-
-    public float moveSpeed; //how many tiles do you move in 1s?
-
     public Inventory inventory;
 
     //TODO: for game testing remove this later
@@ -21,10 +11,6 @@ public class PlayerController : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        map = GameObject.Find("Map").GetComponent<Map>();
-
-        position = map.coordinates[new Vector2Int(0, 0)];
-        target = position;
 
         for (int i = 0; i < 1000; i++) {
             inventory.AddItem(currency);
@@ -33,32 +19,7 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //TODO: we're assuming that we are moving on a square grid here, factor this assumption out into a different input module to allow hex movement. How do we do hex movement on a keyboard? WEADZX?
-        int horizontalMove = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
-        int verticalMove = Mathf.RoundToInt(Input.GetAxis("Vertical"));
-
-        if (target.position.x == position.position.x && target.position.y == position.position.y) {
-            //find the tile coords we're going to and set that as target, if we're moving
-            Vector2Int motion = new Vector2Int(horizontalMove, verticalMove);
-            if (map.InDirection(position, motion)) {
-                target = map.coordinates[position.position + motion];
-            }
-            facing = motion;
-        }
-
-        //drift towards target, snap and set position to it when we're close enough
-        Waypoint targetTile = map.coordinates[target.position];
-        float moveDistance = moveSpeed * Time.deltaTime;
-        Vector3 distance = (targetTile.gameObject.transform.position - gameObject.transform.position);
-        Vector3 movementThisFrame = distance.normalized * moveDistance;
-
-
-        if (distance.sqrMagnitude <= movementThisFrame.sqrMagnitude) {
-            position = target;
-            gameObject.transform.Translate(distance);
-        } else {
-            gameObject.transform.Translate(movementThisFrame);
-        }
+        
 
         if (Input.GetButtonDown("Pause") && Time.timeScale != 0) {
             WindowManager.CreatePauseWindow();          
---
+++ Lanhes Engine/Assets/Scripts/PlayerPawnMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(PlayerController))]
+[RequireComponent (typeof(Rigidbody))]
+public class PlayerPawnMovement : MonoBehaviour
+{
+
+    public float moveSpeed; //how many tiles do you move in 1s?
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: snappng to grip option
+        int horizontalMove = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
+        int verticalMove = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+
+        //TODO barriers I cant go up
+
+        //TODO animation
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontalMove,0,verticalMove)*moveSpeed;
+    }
+}

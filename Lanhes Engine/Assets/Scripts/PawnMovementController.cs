37a51ea24ba0f9a33eea7614fd7fe459e8b7363b
theNimja <theNimja@github.com>
2020-06-22 18:14:04 +0100

    seperated movement out to a new script with the controller seperate; also, the animation is much better now

---
+++ Lanhes Engine/Assets/Scripts/PawnMovementController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class PawnMovementController : MonoBehaviour {
+
+    public float moveSpeed; //how many tiles do you move in 1s?
+
+    private Animator anim;
+    new private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        Vector3 dir = GetInput();
+
+
+        //TODO: snappng to grid option
+
+
+        //TODO barriers I cant go up
+
+        rigidbody.velocity = dir * moveSpeed;
+        Debug.Log(dir);
+        //animate
+        //TODO: end on the last frame?
+        anim.SetInteger("Horizontal", Mathf.RoundToInt(dir.x));
+        anim.SetInteger("Vertical", Mathf.RoundToInt(dir.z));
+        anim.speed = anim.GetInteger("Horizontal") == 0 && anim.GetInteger("Vertical") == 0 ? 0 : 1;
+
+
+    }
+
+    //TODO: maybe instead of abstract classes, use a delegate?
+    internal abstract Vector3 GetInput();
+
+}
--- Lanhes Engine/Assets/Scripts/PlayerPawnMovement.cs
+++ Lanhes Engine/Assets/Scripts/PlayerPawnMovement.cs
@@ -2,43 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent(typeof(PlayerController))]
-[RequireComponent(typeof(Rigidbody))]
-public class PlayerPawnMovement : MonoBehaviour {
-    public float moveSpeed; //how many tiles do you move in 1s?
+public class PlayerPawnMovement : PawnMovementController {
 
-    private Animator anim;
-    new private Rigidbody rigidbody;
-    
-    private void Start() {
-        anim = GetComponent<Animator>();
-        rigidbody = GetComponent<Rigidbody>();
-    }
-    
 
-    // Update is called once per frame
-    void Update() {
+    internal override Vector3 GetInput() {
         //TODO: we have an issue where there is one frame where we can continue play between a dialogue being opened and the next, in which we can move...
         if (WindowManager.instance.ContinuePlay()) {
-            //TODO: snappng to grid option
             int horizontalMove = Mathf.RoundToInt(Input.GetAxis("Horizontal"));
             int verticalMove = Mathf.RoundToInt(Input.GetAxis("Vertical"));
+            return new Vector3(horizontalMove, 0, verticalMove);
 
-            //TODO barriers I cant go up
-
-            rigidbody.velocity = new Vector3(horizontalMove, 0, verticalMove) * moveSpeed;
-
-            //animate
-            //TODO: end up on the last frame?
-            anim.SetInteger("Horizontal", horizontalMove);
-            anim.SetInteger("Vertical", verticalMove);
-            anim.speed = 1;
-        } else {
+    } else {
             //freeze
             //TODO: the movement issue could be simplified if we made movement a product of root motion
-            rigidbody.velocity = Vector3.zero;
-            anim.speed = 0;
+            return Vector3.zero;
 
         }
+
     }
+
 }

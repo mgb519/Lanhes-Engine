63a97d20d011cc6ce6e461fe2c4a830307fa3095
theNimja <theNimja@github.com>
2021-07-16 18:38:10 +0100

    Added skeleton for general case of battle
    Still need a game over screen, and a way to get rewards - although the
    latter may be game-specific.

---
+++ Lanhes Engine/Assets/Scripts/GenericBattle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Runs an instance of the general case of battle, with a victory resulting in rewards and a loss resulting in game over. Should be overriden by games with more battle result states.
+/// </summary>
+public class GenericBattle : MonoBehaviour
+{
+    //TODO decouple trigger from action
+    //TODO this uses no MonoBehaviour methods, so why does it have the extra weight? A friend of mine had this exact problem, I'll ask him when I can get ahold of him...
+    [SerializeField]
+    private IOpponentGroup enemies;
+    void Action() {
+        StartCoroutine(Body());
+    }
+
+
+    IEnumerator Body() {
+        BattleManager.StartBattle(null);
+        yield return new WaitUntil(() => !BattleManager.InBattle()); //wait for the battle to finish before continuing the script.
+        BattleResult outcome = BattleManager.GetResultOfLastBattle();
+        switch (outcome)
+        {
+            case BattleResult.Victory:
+                //TODO give rewards
+                break;
+            case BattleResult.Loss:
+                //TODO game over screen
+                break;
+        }
+
+
+    }
+
+
+}

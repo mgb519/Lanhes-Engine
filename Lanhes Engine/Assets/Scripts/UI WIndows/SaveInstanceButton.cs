289e3514cbc94f377a4068929ffdc22fa10e10d9
theNimja <theNimja@github.com>
2021-09-01 15:16:30 +0100

    Added UI skeletons for save system with dynamic slots

---
+++ Lanhes Engine/Assets/Scripts/UI WIndows/SaveInstanceButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//TODO finish this
+//This should be overriden by the game, depending on how a save slot looks.
+public class SaveInstanceButton : MonoBehaviour
+{
+
+    private string path;
+    public void Clicked() {
+        SendMessageUpwards("SaveSelected", path);
+    }
+
+    public void SetPath(string newPath) {
+        path = newPath;
+        //TODO populate the panel
+    }
+
+}
---
+++ Lanhes Engine/Assets/Scripts/UI WIndows/SaveMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO this class is unfinshed
+public class SaveMenu : MenuWindow
+{
+
+    public SaveInstanceButton template;
+
+    void Awake() {
+        //Time.timeScale = 0f;
+
+        //TODO populate the list
+    }
+
+
+    public void SaveSelected(string path) {
+        //TODO save over the selected slot
+    }
+
+
+    public void SaveNew() { 
+        //TODO save a new slot
+        //TODO what if we want to name,say, slots? 
+    }
+
+}

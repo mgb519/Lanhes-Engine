a175c54b3d3d95ce235ff5fc12a4810c3e1f1295
theNimja <theNimja@github.com>
2021-09-13 12:35:32 +0100

    Reorganisation

--- Lanhes Engine/Assets/Scripts/UI WIndows/InventoryViewWindow.cs
+++ Lanhes Engine/Assets/Scripts/UI WIndows/InventoryViewWindow.cs
@@ -3,24 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class InventoryViewWindow : MenuWindow {
+public abstract class InventoryViewWindow : MenuWindow
+{
     public SelectionButton buttonTemplate;
 
     public Transform contentWindow;
 
-    private float newButtonPos;
-    public float yPadFromTop = 10;
-    public float xPadFromLeft = 10;
 
     //Set this when you spawn one.
     public Inventory inventory;
-
-    //TODO: perhaps we only need one tag in practise?
-    public List<InventoryItem.Tags> filter = new List<InventoryItem.Tags>();
-
-    public void Awake() {
-        newButtonPos = yPadFromTop;
-    }
 
     public abstract void HandleSelection(InventoryItem selected);
 
@@ -35,14 +26,14 @@ public abstract class InventoryViewWindow : MenuWindow {
         //render each item
         foreach (InventoryItem item in inventory.Contents().Keys) {
             //check that every tag we are filtering for is present
-            if (filter.TrueForAll(x => item.tags.Contains(x))) {
-                SelectionButton button = item.Render();
-                button.gameObject.transform.SetParent(GameObject.Find("Content").transform);
-            }
+
+            SelectionButton button = item.Render();
+            button.gameObject.transform.SetParent(GameObject.Find("Content").transform);
+
         }
     }
 
-    
+
 
 
 }

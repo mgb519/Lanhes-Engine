99abea17b410fb06269d88c02d0ef6ebd5637807
theNimja <theNimja@github.com>
2022-04-20 19:08:11 +0100

    Item names should be localized now

--- Lanhes Engine/Assets/Scripts/InventoryItem.cs
+++ Lanhes Engine/Assets/Scripts/InventoryItem.cs
@@ -1,20 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 using UnityEngine.UI;
 
 
 //[CreateAssetMenu(fileName = "New Item" ,menuName ="Inventory Item")]
 public abstract class InventoryItem : ScriptableObject,ISelectable {
     public string systemName;
-    public string readableName;
+    public LocalizedString readableName;
     public int maxStack;
 
     public virtual SelectionButton Render() { //TODO your game *will* override this
         SelectionButton button = GameObject.Instantiate(WindowManager.BaseButton());
-        button.GetComponentInChildren<Text>().text = readableName;
+        button.GetComponentInChildren<Text>().text = readableName.GetLocalizedString();
         button.dat = this;
         return button;
     }
+
+
 
 }
